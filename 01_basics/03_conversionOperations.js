1bcdf4bd3adc5bf7fbba4ee076fdf405812d4e79
dhavalsinh07 <162587865+dhavalsinh07@users.noreply.github.com>
2024-06-20 06:24:49 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+const score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0;
+
+let isloggedIn = "abc";
+
+let booleanIsLoggedIn = Boolean(isloggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "abc" => true
+
+let someNumber = 33;
+
+let stringNum = String(someNumber);
+
+console.log(stringNum);
+console.log(typeof stringNum);
