292ca349748751220a590db5ec42df3da990d99d
dhavalsinh07 <162587865+dhavalsinh07@users.noreply.github.com>
2024-06-21 20:04:12 +0000

    Nums and Maths

---
+++ 01_basics/06_Nums_and_Math.js
@@ -0,0 +1,38 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(typeof balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNum = 122.8984984
+
+// console.log(otherNum.toPrecision(3));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++++  Maths  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(23.23));
+// console.log(Math.round(23.98));4
+// console.log(Math.ceil(5.4));
+// console.log(Math.floor(5.6));
+
+// console.log(Math.min(2,4,6,12,1));
+// console.log(Math.max(2,4,6,12,1));
+
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
