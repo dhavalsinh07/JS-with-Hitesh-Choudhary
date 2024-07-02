4c8e0adbebfd16162b2f85a29f48a8894897fb4d
Dhavalsinh Solanki <162587865+dhavalsinh07@users.noreply.github.com>
2024-07-02 14:06:02 +0000

    do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+
+// let index = 0
+// while (index <= 10) {
+//     // console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArr = ["flash", "batman", "superman"]
+
+// let arr = 0
+// while (arr < myArr.length) {
+//     console.log(`value is ${myArr[arr]}`);
+//     arr++
+// }
+
+let score = 11                        // if here we add 11 that is higher than the 10 so it not folloow the condition but in these
+                                        // case score is 11 print // the reason of these is that in do while loop (do (work)) is come first so that's why it print a 11 value of score.
+
+do {
+    console.log(`score is ${score}`)
+    score ++
+} while (score <= 10);
